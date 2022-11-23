37870691cf8435a211bed46766777b2a40dcf0e0
DonatellaC <donatella.carta@mailbox.org>
2022-11-23 17:50:41 +0100

    add BookShelfChanger component

---
+++ starter/src/components/BookShelfChanger/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const BookShelfChanger = ({ handleChange, selectedOption }) => {
+  return (
+    <div className="book-shelf-changer">
+      <select value={selectedOption} onChange={handleChange}>
+        <option value="none" disabled>
+          Move to...
+        </option>
+        <option value="currentlyReading">Currently Reading</option>
+        <option value="wantToRead">Want to Read</option>
+        <option value="read">Read</option>
+        <option value="none">None</option>
+      </select>
+    </div>
+  );
+};
+
+export default BookShelfChanger;
